ba19561042f5f5c30b5871acfc333598725c044e
Mircho Mirev <m.mirev@allterco.com>
2022-11-27 11:03:45 +0200

    ble-ruuvi w/ packed struct implementation

--- ble-ruuvi.js
+++ ble-ruuvi.js
@@ -1,6 +1,3 @@
-// For ruuvi data format see
-// https://docs.ruuvi.com/communication/bluetooth-advertisements/data-format-5-rawv2
-
 let CONFIG = {
   scan_duration: BLE.Scanner.INFINITE_SCAN,
   temperature_thr: 18,
@@ -9,90 +6,104 @@ let CONFIG = {
   event_name: "ruuvi.measurement",
 };
 
-let RUUVI_MFD_ID_LE = 0x9904;
+let RUUVI_MFD_ID = 0x0499;
 let RUUVI_DATA_FMT = 5;
 
+//format is subset of https://docs.python.org/3/library/struct.html
+let packedStruct = {
+  buffer: '',
+  setBuffer: function(buffer) {
+    this.buffer = buffer;
+  },
+  utoi: function(u16) {
+    return (u16 & 0x8000) ? u16 - 0x10000 : u16;
+  },
+  getUInt8: function() {
+    return this.buffer.at(0)
+  },
+  getInt8: function() {
+    let int = this.getUInt8();
+    if(int & 0x80) int = int - 0x100;
+    return int;
+  },
+  getUInt16LE: function() {
+    return 0xffff & (this.buffer.at(1) << 8 | this.buffer.at(0));
+  },
+  getInt16LE: function() {
+    return this.utoi(this.getUInt16LE());
+  },
+  getUInt16BE: function() {
+    return 0xffff & (this.buffer.at(0) << 8 | this.buffer.at(1));
+  },
+  getInt16BE: function() {
+    return this.utoi(this.getUInt16BE(this.buffer));
+  },
+  unpack: function(fmt, keyArr) {
+    let b = '<>!';
+    let le = fmt[0] === '<';
+    if(b.indexOf(fmt[0]) >= 0) {
+      fmt = fmt.slice(1);
+    }
+    let pos = 0;
+    let jmp;
+    let bufFn;
+    let res = {};
+    while(pos<fmt.length && pos<keyArr.length && this.buffer.length > 0) {
+      jmp = 0;
+      bufFn = null;
+      if(fmt[pos] === 'b' || fmt[pos] === 'B') jmp = 1;
+      if(fmt[pos] === 'h' || fmt[pos] === 'H') jmp = 2;
+      if(fmt[pos] === 'b') {
+        res[keyArr[pos]] = this.getInt8();
+      }
+      else if(fmt[pos] === 'B') {
+        res[keyArr[pos]] = this.getUInt8();
+      }
+      else if(fmt[pos] === 'h') {
+        res[keyArr[pos]] = le ? this.getInt16LE() : this.getInt16BE();
+      }
+      else if(fmt[pos] === 'H') {
+        res[keyArr[pos]] = le ? this.getUInt16LE() : this.getUInt16BE();
+      }
+      this.buffer = this.buffer.slice(jmp);
+      pos++;
+    }
+    return res;
+  }
+};
+
 let RuuviParser = {
-  getUInt16BE: function (bytes) {
-    return (bytes.at(0) << 8) | bytes.at(1);
-  },
-
-  getInt16BE: function (bytes) {
-    let num = this.getUInt16BE(bytes);
-    //two's complement
-    if (num & 0x8000) num = num - 0x10000;
-    return num;
-  },
-
-  //3,4
-  getTemperature: function (mfd) {
-    return this.getInt16BE(mfd.slice(3)) * 0.005;
-  },
-
-  getHumidity: function (mfd) {
-    return this.getUInt16BE(mfd.slice(5)) * 0.0025;
-  },
-
-  getPressure: function (mfd) {
-    return this.getUInt16BE(mfd.slice(7)) + 50000;
-  },
-
-  getAccelXYZ: function (mfd) {
-    return {
-      x: this.getInt16BE(mfd.slice(9)),
-      y: this.getInt16BE(mfd.slice(11)),
-      z: this.getInt16BE(mfd.slice(13)),
-    };
-  },
-
-  getBatteryMv: function (mfd) {
-    let power_info = this.getUInt16BE(mfd.slice(15));
-    let battery_mv = (power_info >> 5) + 1600;
-    return battery_mv;
-  },
-
-  getTxPower: function (mfd) {
-    return -40 + (mfd.at(16) & 0x1f) * 2;
-  },
-
-  getMovementCounter: function (mfd) {
-    return mfd.at(17);
-  },
-
-  getSequence: function (mfd) {
-    return this.getUInt16BE(mfd.slice(18));
-  },
-
-  getMac: function (mfd) {
-    let mac = mfd.slice(20, 26);
-    print(mac.length);
-    return mac;
-  },
-
   getData: function (res) {
     let data = BLE.GAP.ParseManufacturerData(res.advData);
     if (typeof data !== "string" || data.length < 26) return null;
-    //Manufacturer ID is at the beginning of the result
-    if (this.getUInt16BE(data) !== RUUVI_MFD_ID_LE) return null;
-    if (data.at(2) !== RUUVI_DATA_FMT) {
+    packedStruct.setBuffer(data);
+    let hdr = packedStruct.unpack('<HB', ['mfd_id', 'data_fmt']);
+    if(hdr.mfd_id !== RUUVI_MFD_ID) return null;
+    if(hdr.data_fmt !== RUUVI_DATA_FMT) {
       print("unsupported data format from", res.addr);
       print("expected format", RUUVI_DATA_FMT);
       return null;
-    }
-    let m = {
-      temperature: this.getTemperature(data),
-      humidity: this.getHumidity(data),
-      pressure: this.getPressure(data),
-      accel: this.getAccelXYZ(data),
-      battery_mv: this.getBatteryMv(data),
-      tx_power: this.getTxPower(data),
-      movement_counter: this.getMovementCounter(data),
-      sequence: this.getSequence(data),
-      //mac: this.getMac(data),
-      addr: res.addr.slice(0, -2),
-      rssi: res.rssi,
     };
-    return m;
+    let rm = packedStruct.unpack('>hHHhhhHBHBBBBBB', [
+      'temp',
+      'humidity',
+      'pressure',
+      'acc_x',
+      'acc_y',
+      'acc_z',
+      'pwr',
+      'cnt',
+      'sequence',
+      'mac_0','mac_1','mac_2','mac_3','mac_4','mac_5'
+    ]);
+    rm.temp = rm.temp * 0.005;
+    rm.humidity = rm.humidity * 0.0025;
+    rm.pressure = rm.pressure + 50000;
+    rm.batt = (rm.pwr >> 5) + 1600;
+    rm.tx = (rm.pwr & 0x001f * 2) - 40;
+    rm.addr = res.addr.slice(0, -2);
+    rm.rssi = res.rssi;
+    return rm;
   },
 };
 
@@ -108,7 +119,7 @@ function emitOverWs(measurement) {
 }
 
 function triggerAutomation(measurement) {
-  if (measurement.temperature < CONFIG.temperature_thr) {
+  if (measurement.temp < CONFIG.temperature_thr) {
     // turn the heater on
     Shelly.call("Switch.Set", { id: CONFIG.switch_id, on: true });
   }
