b191020ddfb8c8557d48fafe305dfa3aa2c0b206
taulfsime <kris.td@icloud.com>
2023-08-12 23:25:20 +0300

    Interface functions for kvs

--- control-with-telegram.js
+++ control-with-telegram.js
@@ -2,6 +2,34 @@ let key = "";
 let url = "https://api.telegram.org/bot" + key + "/getUpdates";
 let offset = 0;
 
+let CONFIG = {
+  
+};
+
+let KVS = {
+  load: function (key, callback) {
+    Shelly.call(
+      "KVS.Get", 
+      { key: key },
+      function (data, error, message, kvsObject) {
+        if(error !== 0) {
+          console.log("Cannot read the value for the provided key, reason:", message);
+          return;
+        }
+        kvsObject[key] = data.value;
+        if(callback) {
+          callback();
+        }
+      },
+      this
+    );
+  },
+
+  write: function (key, value) {
+    Shelly.call("KVS.Set", { key: key, value: value } );
+  }
+};
+
 Shelly.call("HTTP.GET", {
   url: url
 }, function(d, r) {
@@ -13,3 +41,9 @@ Shelly.call("HTTP.GET", {
   //offset = d.body.result[0].update_id + 1;
   //console.log(offset, d.body.result[0].text);
 });
+
+function printTest() {
+  console.log(KVS.test);
+}
+
+KVS.load("test", printTest);
