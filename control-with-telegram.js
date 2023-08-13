794e7f22c8d5e365b33a7fee7005715303d2f15e
taulfsime <kris.td@icloud.com>
2023-08-13 15:48:43 +0300

    Start reading messages

--- control-with-telegram.js
+++ control-with-telegram.js
@@ -1,6 +1,13 @@
 let CONFIG = {
   timeout: 15, //in seconds
   eventName: "telegram-bot", //if you have more than once instace of this script, you should set a unique event name for each
+  commands: {
+    "/test": {
+      action: function (args) {
+        console.log(args);
+      }
+    }
+  }
 };
 
 let KVS = {
@@ -28,9 +35,14 @@ let KVS = {
 };
 
 let TelegramBot = {
-  init: function (botKey, messageOffset) {
+  botKey: undefined,
+  messageOffset: undefined,
+  onLastMessage: undefined, // (lastMessageId)
+
+  init: function (botKey, messageOffset, onLastMessage) {
     this.botKey = botKey;
     this.messageOffset = messageOffset;
+    this.onLastMessage = onLastMessage;
 
     this.startNewPoll();
   },
@@ -40,34 +52,79 @@ let TelegramBot = {
     Shelly.call(
       "HTTP.REQUEST",
       { 
-        method: "GET",
+        method: "POST",
         url: "https://api.telegram.org/bot" + this.botKey + "/getUpdates", 
-        timeout: CONFIG.timeout 
+        timeout: CONFIG.timeout,
+        body: {
+          offset: this.messageOffset + 1
+        }
       },
       this.onFinishPoll,
       this
     );
   },
 
-  onFinishPoll: function (data, error, message, self) {
-    console.log("Received", data);
+  /* callback */
+  onFinishPoll: function (data, error, errorMessage, self) {
+    console.log(self);
     if(error !== 0) {
-      console.log("Poll finishes with error ->", message);
+      console.log("Poll finishes with error ->", errorMessage);
       return;
     }
 
+    let lastUpdateId = -1;
     let response = JSON.parse(data.body);
-    for (let res of response.result) {
-      console.log(res);
+    if(response.result.length === 0) {
+      console.log("No new messages");
+      return;
     }
 
-    console.log("hnn");
+    for (let res of response.result) {
+      console.log(JSON.stringify(res));
+      self.handleMessage(res.message);
+      lastUpdateId = res.update_id;
+    }
+
+    if(typeof self.onLastMessage === "function") {
+      self.onLastMessage(lastUpdateId);
+    }
+  },
+
+  handleMessage: function (message) {
+    this.sendMessage(message.chat.id, message.text);
+  },
+
+  sendMessage: function(chatId, message) {
+    Shelly.call(
+      "HTTP.REQUEST",
+      { 
+        method: "POST",
+        url: "https://api.telegram.org/bot" + this.botKey + "/sendMessage", 
+        timeout: CONFIG.timeout,
+        body: {
+          chat_id: chatId,
+          text: message
+        }
+      },
+      this.onFinishPoll,
+      this
+    );
   },
 
   startNewPoll: function () {
     Shelly.emitEvent(CONFIG.eventName);
   },
 };
+
+function onLastMessage (lastUpdateId) {
+  console.log(lastUpdateId);
+
+  if (lastUpdateId <= KVS.messageOffset) {
+    return;
+  }
+
+  KVS.write("messageOffset", lastUpdateId);
+}
 
 function init () {
   if(typeof KVS.botKey !== "string" || typeof KVS.messageOffset !== "number") {
@@ -77,7 +134,6 @@ function init () {
 
   console.log("Data is loaded");
   Shelly.addEventHandler(function(data) {
-    console.log("new event", data.info.event);
     if(
       typeof data === "undefined" || 
       typeof data.info === "undefined" ||
@@ -89,7 +145,7 @@ function init () {
     TelegramBot.onEvent();
   });
 
-  TelegramBot.init(KVS.botKey, KVS.messageOffset);
+  TelegramBot.init(KVS.botKey, KVS.messageOffset, onLastMessage);
 }
 
 KVS.load("botKey", init);
