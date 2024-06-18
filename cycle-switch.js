67236b6864739a61d2400022e3d97ccbcb78a33b
Radoslav Sapundzhiev <radoslav.sapundzhiev@shelly.com>
2024-06-18 16:14:01 +0300

    improve cycling device output state

--- cycle-switch.js
+++ cycle-switch.js
@@ -1,57 +1,89 @@
-let CONFIG = {
-  /**
-   * Pick your desired Input to be used for triggering the cycling (note: this input would be changed
-   * to detached!)
-   */
-  INPUT_ID: 0,
+const CONFIG = {
+    /**
+     * Pick your desired Input to be used for triggering the cycling (note: this input would be changed
+     * to detached and to type button!)
+     */
+    INPUT_ID: 0,
 
-  /**
-   * List (in the expected order) the operations that you want this script to cycle through.
-   * E.g. [switchId, "on" or "off"]
-   */
-  CYCLES: [
-    [0, "on"],
-    [1, "on"],
-    [0, "off"],
-    [1, "off"],
-  ],
+    /**
+     * List (in the expected order) the operations that you want this script to cycle through.
+     * E.g. for 3 outputs - alternating on/off it could be [true, false, false] on the first cycle, 
+     * and [false, true, false] for the second cycle and so on ...
+     * Value true means that output is enabled.
+     * Value false means that output is disabled.
+     * When cycle gets to the end of CYCLES array - on the next iteration operations start from beggining.
+     * With every single item in the inner arrays with values true/false output state can be managed - on/off. 
+     * CYCLES array could be fit to output count of the shelly device.
+     * E.g. device with 3 outputs 
+     * CYCLES: [
+            [true, false, false],
+            [false, true, false],
+            [false, false, true],
+            [false, false, false]
+        ]
+     * E.g. device with 4 outputs 
+     * CYCLES: [
+            [true, false, false, false],
+            [false, true, false, false],
+            [false, false, true, false],
+            [false, false, false, true],
+            [false, false, false, false]
+        ],
+        ...
+     */
+    CYCLES: [
+        [true, false, false],
+        [false, true, false],
+        [false, false, true],
+        [false, false, false],
+    ],
 };
 
 let currentCycle = 0;
 
-let runCycle = function () {
-  let currentOperation = CONFIG.CYCLES[currentCycle];
-
-  if (!currentOperation) {
-    currentCycle = 0;
-    currentOperation = CONFIG.CYCLES[currentCycle];
-  }
-  currentCycle++;
-
-  Shelly.call("switch.set", {
-    id: JSON.stringify(currentOperation[0]),
-    on: currentOperation[1] === "on",
-  });
-};
-
-let setup = function () {
-  Shelly.call(
-    "switch.setconfig",
-    { id: JSON.stringify(CONFIG.INPUT_ID), config: { in_mode: "detached" } },
-    function () {
-      Shelly.addEventHandler(function (event) {
-        if (event.component === "input:" + JSON.stringify(CONFIG.INPUT_ID)) {
-          if (
-            event.info.state !== false &&
-            event.info.event !== "btn_up" &&
-            event.info.event !== "btn_down"
-          ) {
-            runCycle();
-          }
-        }
-      }, null);
+function runCycle () {
+    if (currentCycle === CONFIG.CYCLES.length) {
+      currentCycle = 0;
     }
-  );
+
+    let currentOperation = CONFIG.CYCLES[currentCycle];
+
+    currentCycle++;
+
+    for (let input = 0; input < currentOperation.length; input++) {
+        Shelly.call(
+            "Switch.Set", 
+            {
+                id: JSON.stringify(input),
+                on: currentOperation[input]
+            }
+            
+        );
+    }
 };
+
+function setup() {
+    Shelly.call(
+        "Switch.SetConfig", 
+        {
+            id: JSON.stringify(CONFIG.INPUT_ID), 
+            config: {in_mode: "detached" }
+        }
+    );
+
+    Shelly.call(
+        "Input.SetConfig",
+        {
+            id:  JSON.stringify(CONFIG.INPUT_ID),
+            config: {type: "button"}
+        }
+    );
+}
 
 setup();
+
+Shelly.addEventHandler(function (event) {
+    if (event.info.event === "single_push" && event.info.id === CONFIG.INPUT_ID) {
+        runCycle();
+    }
+});
