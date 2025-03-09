16fc2b54a9140d2c855dd1d039388b81457ee81f
taulfsime <kris.td@icloud.com>
2025-03-09 13:31:19 +0200

    lora: add send and receive examples
    Signed-off-by: taulfsime <kris.td@icloud.com>

---
+++ lora-lightcontrol-sender/README.md
@@ -0,0 +1,43 @@
+# Send Scheduled Messages to Shelly 0/1-10V DimmerG3
+
+## Short Description
+
+This setup involves two scripts:
+
+- The **initiator script** creates a schedule that executes a function and emits an event.
+- The **sender script** listens for this event and sends an encrypted message to one or more `Shelly 0/1-10V DimmerG3` devices, controlling their state and brightness.
+
+## Requirements
+
+- One Shelly device that supports the **LoRa add-on**  
+- A **unique AES key** for encrypting messages  
+  - The provided key is only an example  
+  - Generate your own [here](https://generate-random.org/encryption-key-generator)
+
+## Installation
+
+1. Wire up your Shelly devices  
+2. Attach the LoRa add-ons to the Shelly device  
+3. Power on the device  
+4. In the embedded web interface, open the **Add-on** submenu and enable the **LoRa add-on**
+
+---
+
+### Script: Initiator
+
+1. Create a script named `lora-lightcontrol-initiator.js`  
+2. This script uses:
+   - `TIME_ENUM` to define scheduled entries, including:
+     - Hour of execution
+     - Desired on/off state
+     - Brightness level
+   - `TIMESPEC` to define cron-like execution timing, which aligns with the `TIME_ENUM` values
+
+> **Note:** After saving and running the script for the first time, modifying `TIME_ENUM` will also require updating the associated schedule.
+
+---
+
+### Script: Sender
+
+1. Create a script named `lora-lightcontrol-sender.js`  
+2. This script listens for events emitted by the initiator and sends encrypted messages to target DimmerG3 devices to update their state and brightness
---
+++ lora-lightcontrol-sender/lora-lightcontrol-initiator.js
@@ -0,0 +1,90 @@
+const SCRIPT_ID = Shelly.getCurrentScriptId();
+
+//key is time, value is device number, 
+const TIME_ENUM = {
+  "05:00": { enable: true, brightness: 50 },
+  "07:00": { enable: false, brightness: 0 },
+  "20:00": { enable: true, brightness: 50 },
+  "23:00": { enable: true, brightness: 100 }
+};
+
+//timespec based on the time enum
+const TIMESPEC = '0 0 5,7,20,23 * * *';
+
+//schedule identifier
+const ORIGIN = 'script-' + SCRIPT_ID;
+
+function emitMessage() {
+  Shelly.call(
+    'Sys.GetStatus',
+    null,
+    function(data, err_code, err_msg) {
+      if (err_code !== 0) {
+        console.log('Error:', err_code, err_msg);
+        return;
+      }
+
+      if (typeof TIME_ENUM[data.time] === 'undefined') {
+        console.log('Error: No handler for ', data.time);
+      }
+
+      const obj = TIME_ENUM[data.time];
+      const string = ((obj.enable | 0) + '' + (obj.brightness));
+
+      Shelly.emitEvent(
+        'lora_send_message',
+        { message: string }
+      );
+    }
+  );
+}
+
+function createSchedule() {
+  Shelly.call(
+    'Schedule.Create',
+    {
+      enable: true,
+      timespec: TIMESPEC,
+      calls: [
+        {
+          origin: ORIGIN,
+          method: 'Script.Eval',
+          params: { id: SCRIPT_ID, code: 'emitMessage()' }
+        }
+      ]
+    },
+    function(_, err_code, err_msg) {
+      if (err_code !== 0) {
+        console.log('Error:', err_code, err_msg);
+      }
+    }
+  );
+}
+
+function init() {
+  Shelly.call(
+    'Schedule.List',
+    null,
+    function(data, err_code, err_msg) {
+      if (err_code !== 0) {
+        console.log('Error:', err_code, err_msg);
+        return;
+      }
+
+      let hasSchedule = false;
+
+      for (let i = 0; i < data.jobs.length; i++) {
+        if (data.jobs[i].calls[0].origin && data.jobs[i].calls[0].origin === ORIGIN) {
+          hasSchedule = true;
+          break;
+        }
+      }
+
+      if (!hasSchedule) {
+        createSchedule();
+      }
+    }
+  );
+}
+
+init();
